52e67baaf845361f48a3895ac7acdfbefcb8117b
Mike <shelliha@s21-edu.ru>
2025-12-10 19:53:36 +0300

    добавлена возможность задавать по умолчанию произвольное количество int-аргументов

--- headers/Wrapper.h
+++ headers/Wrapper.h
@@ -6,34 +6,68 @@
 
 #include <memory>
 
-template<typename ClassName, typename ReturnType>
+template<typename ClassName, typename ReturnType, typename ... Args>
 class Wrapper : public IWrapper
 {
     private:
 
-        std::function<ReturnType(ClassName&)> _function;
+        std::function<ReturnType(ClassName*, Args...)> _function;
 
         /*
-        Из ТЗ подразумевается, что оборачиваемый объект, вероятнее всего,
-        создаётся на стеке. Поэтому, умными указателями можно пренебречь.
+            Объект, чья функция оборачиваися, может создаваться и на стеке. shared_ptr не применим.
+            Лучше даже будем кидать исключение в случае, когда пытается вызваться функция из удалённого объекта.
         */ 
         ClassName * _subj;
+
+        std::map<std::string, int> _default_arguments;
+
+
+        void callWithDefaultArgs() {
+            if constexpr (sizeof...(Args) == 0) {
+                _function(_subj);
+            } else {
+                std::array<int, sizeof...(Args)> args_array;
+                
+                for (size_t i = 0; i < sizeof...(Args); ++i) {
+                    std::string key = "arg" + std::to_string(i + 1);
+                    args_array[i] = _default_arguments.at(key);
+                }
+                
+                callWithArray(args_array);
+            }
+        }
+
+        void callWithArray(const std::array<int, sizeof...(Args)>& args) {
+            auto call_func = [this, &args]<size_t... Is>(std::index_sequence<Is...>) {
+                _function(_subj, args[Is]...);
+            };
+            
+            call_func(std::index_sequence_for<Args...>{});
+        }
 
     public:
 
 
-        Wrapper(ClassName* subj, ReturnType (ClassName::*func)())
-            : _subj(subj) 
+        Wrapper(ClassName* subj, ReturnType (ClassName::*func)(Args...),
+        const std::map<std::string, int>& default_arguments)
+            : _subj(subj), _default_arguments(default_arguments) 
         {
-            _function = [subj, func](ClassName& obj) -> ReturnType {
-                return (obj.*func)();
+            
+            _function = [func](ClassName* obj, Args... args) -> ReturnType {
+                return (obj->*func)(args...);
             };
         }
 
         void execute() override {
-            _function(*_subj);
+            callWithDefaultArgs();
         }
 
 };
 
-
+//TODO list
+/*
+1. Сделать проверку, что в словарь подаётся ровно столько аргументов, сколько их в обарачиваемой функции
+2. Хочется, чтобы можно было подавать любой типа данных.
+3. Разобраться - на стеке или куче будет создаваться обёртка
+4. Добавить возможность передавать аргументы после регистрации.
+*/
