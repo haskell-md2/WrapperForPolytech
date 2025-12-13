1ccb4ad48114180d947039f0cdecbc7738d0d771
Mike <shelliha@s21-edu.ru>
2025-12-13 11:22:38 +0300

    проверка существования команды

--- headers/Engine.h
+++ headers/Engine.h
@@ -15,11 +15,17 @@ public:
 
     ExecuteResult execute(const std::string& command_name) {
         auto it = commands_.find(command_name);
+        if (it == commands_.end()) {
+            throw std::runtime_error("Команда не найдена: " + command_name);
+        }
         return ExecuteResult(it->second->execute());
     }
 
     ExecuteResult execute(const std::string& command_name, const std::map<std::string, std::any>& args_map) {
         auto it = commands_.find(command_name);
+        if (it == commands_.end()) {
+            throw std::runtime_error("Команда не найдена: " + command_name);
+        }
         return ExecuteResult(it->second->execute(args_map));
     }
 
