fd10e040536e8e7304269b3684ff978ed51cf963
Mike <shelliha@s21-edu.ru>
2025-12-14 14:05:27 +0300

    Clang для ExecuteResult

--- headers/ExecuteResult.h
+++ headers/ExecuteResult.h
@@ -42,11 +42,10 @@ class ExecuteResult {
 
     void print(std::ostream &os) const { printer_(os, result_); }
 
-
-    ExecuteResult(const ExecuteResult&) = default;
-    ExecuteResult(ExecuteResult&&) noexcept = default;
-    ExecuteResult& operator=(const ExecuteResult&) = default;
-    ExecuteResult& operator=(ExecuteResult&&) noexcept = default;
+    ExecuteResult(const ExecuteResult &) = default;
+    ExecuteResult(ExecuteResult &&) noexcept = default;
+    ExecuteResult &operator=(const ExecuteResult &) = default;
+    ExecuteResult &operator=(ExecuteResult &&) noexcept = default;
     ~ExecuteResult() = default;
 };
 
